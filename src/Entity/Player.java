40a36a14c6b35a8ec403a5bc901ecfc16981d2a4
Pasquale Celella <pasquale.celella@gmail.com>
2018-11-21 22:07:58 +0100

    Firing and a bit better code

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -14,7 +14,7 @@ public class Player extends MapObject {
     // Each element of the array refers to the number of frames (or columns) in a row of the sprite asset
     // The sprite asset now have 3 rows (to animate 3 states) so I'm defining the number of frames of each row
     private final int[] numFrames = {6,12,12};
-	
+
     // States
     // The integer refers to the row of the sprite asset
     private static final int IDLE = 0;
@@ -30,78 +30,78 @@ public class Player extends MapObject {
     private int currentProjectile = 0;
 
     /* not now
-	private int health;
-	private int maxHealth;
-	private boolean dead;
-	*/
-	
-	public Player(TileMap tm) {
-		super(tm);
+    private int health;
+    private int maxHealth;
+    private boolean dead;
+    */
 
-		width = 32; // width of the sprite image
-		height = 32; // height of the sprite image
-		cwidth = 20; // width of the collision box
-		cheight = 20; // height of the collision box
+    public Player(TileMap tm) {
+        super(tm);
+
+        width = 32; // width of the sprite image
+        height = 32; // height of the sprite image
+        cwidth = 20; // width of the collision box
+        cheight = 20; // height of the collision box
 
         // Movement parameters
-		moveSpeed = 0.3;
-		maxSpeed = 1.6;
-		stopSpeed = 0.4;
-		fallSpeed = 0.15;
-		maxFallSpeed = 4.0;
-		jumpStart = -4.8;
-		stopJumpSpeed = 0.3;
+        moveSpeed = 0.3;
+        maxSpeed = 1.6;
+        stopSpeed = 0.4;
+        fallSpeed = 0.15;
+        maxFallSpeed = 4.0;
+        jumpStart = -4.8;
+        stopJumpSpeed = 0.3;
 
-		try {
-			BufferedImage spritesheet = ImageIO.read(getClass().getResourceAsStream("/Pirates/pirates.png"));
-			sprites= new ArrayList<BufferedImage[]>();
-			for(int i=0; i<3; i++) { // i = number of row
-				BufferedImage[] bi= new BufferedImage[numFrames[i]];
-				for(int j=0; j<numFrames[i]; j++) { // j = number of column
-						bi[j] = spritesheet.getSubimage(j*width, i*height, width, height);
-				}
-				sprites.add(bi);
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+        try {
+            BufferedImage spritesheet = ImageIO.read(getClass().getResourceAsStream("/Pirates/pirates.png"));
+            sprites= new ArrayList<BufferedImage[]>();
+            for(int i=0; i<3; i++) { // i = number of row
+                BufferedImage[] bi= new BufferedImage[numFrames[i]];
+                for(int j=0; j<numFrames[i]; j++) { // j = number of column
+                        bi[j] = spritesheet.getSubimage(j*width, i*height, width, height);
+                }
+                sprites.add(bi);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
 
         facingRight = true;
         projectiles=new ArrayList<Projectile>();
         currentAction = IDLE;
-		animation = new Animation();
-		animation.setFrames(sprites.get(IDLE));
-		animation.setDelay(100);
-	}
+        animation = new Animation();
+        animation.setFrames(sprites.get(IDLE));
+        animation.setDelay(100);
+    }
 
-	public void setFiring(boolean fire) {
-		firing = fire;
-	}
-	
-	private void getNextPosition() {
-		// Movement
-		if(left) {
-			dx -= moveSpeed; // speed increases progressively
-			if(dx < -maxSpeed)
-				dx = -maxSpeed; // max speed reached
-		}
-		else if(right) {
-			dx += moveSpeed;
-			if(dx > maxSpeed)
-				dx = maxSpeed;
-		}
-		else { // I'm not moving
-			if(dx > 0) {
-				dx -= stopSpeed;
-				if(dx < 0)
-					dx = 0;
-			}
-			else if(dx < 0) {
-				dx += stopSpeed;
-				if(dx > 0)
-					dx = 0;
-			}
-		}
+    public void setFiring(boolean fire) {
+        firing = fire;
+    }
+
+    private void getNextPosition() {
+        // Movement
+        if(left) {
+            dx -= moveSpeed; // speed increases progressively
+            if(dx < -maxSpeed)
+                dx = -maxSpeed; // max speed reached
+        }
+        else if(right) {
+            dx += moveSpeed;
+            if(dx > maxSpeed)
+                dx = maxSpeed;
+        }
+        else { // I'm not moving
+            if(dx > 0) {
+                dx -= stopSpeed;
+                if(dx < 0)
+                    dx = 0;
+            }
+            else if(dx < 0) {
+                dx += stopSpeed;
+                if(dx > 0)
+                    dx = 0;
+            }
+        }
 
         // Jumping
         if(jumping &&!falling) {
@@ -121,16 +121,16 @@ public class Player extends MapObject {
                 dy = maxFallSpeed;
         }
 
-		// Cannot attack while moving
-		if((currentAction== SHOOTING)&&!(jumping||falling)) {
-			dx = 0;
-		}
-	}
+        // Cannot attack while moving
+        if((currentAction== SHOOTING)&&!(jumping||falling)) {
+            dx = 0;
+        }
+    }
 
-	public void update() {
-		getNextPosition(); // Update position
-		checkTileMapCollision();
-		setPosition(xtemp, ytemp);
+    public void update() {
+        getNextPosition(); // Update position
+        checkTileMapCollision();
+        setPosition(xtemp, ytemp);
 
         if(right)
             facingRight = true;
@@ -149,23 +149,23 @@ public class Player extends MapObject {
             currentProjectile = 0; // I'm not firing anymore... reset the number of projectiles created
 
         // Update projectiles on the screen and check if they must be removed
-		for (int i=0; i< projectiles.size();i++) {
-			projectiles.get(i).update(); // Update projectile position
-			if(projectiles.get(i).shouldRemove()) {
-				projectiles.remove(i);
-				i--;
-			}
-		}
+        for (int i=0; i< projectiles.size();i++) {
+            projectiles.get(i).update(); // Update projectile position
+            if(projectiles.get(i).shouldRemove()) {
+                projectiles.remove(i);
+                i--;
+            }
+        }
 
-		// Set sprite animation
-		if(left || right) {
-			if(currentAction != WALKING) {
-				currentAction = WALKING;
-				animation.setFrames(sprites.get(WALKING));
-				animation.setDelay(100);
-			}
-		}
-		else if(jumping) {
+        // Set sprite animation
+        if(left || right) {
+            if(currentAction != WALKING) {
+                currentAction = WALKING;
+                animation.setFrames(sprites.get(WALKING));
+                animation.setDelay(100);
+            }
+        }
+        else if(jumping) {
             if (currentAction != JUMPING) {
                 currentAction = JUMPING;
                 animation.setFrames(sprites.get(JUMPING));
@@ -192,24 +192,24 @@ public class Player extends MapObject {
         }
 
 
-		animation.update();
-	}
-	
-	public void draw(Graphics2D g) {
-		setMapPosition(); // update xmap and ymap
+        animation.update();
+    }
 
-		// draw flaming projectiles
-		for (int i=0; i < projectiles.size(); i++){
-			projectiles.get(i).draw(g);
-		}
+    public void draw(Graphics2D g) {
+        setMapPosition(); // update xmap and ymap
 
-		// draw player
-		if(facingRight) {
-			g.drawImage(animation.getImage(), (int)(x+xmap-width/2), (int)(y+ymap-height/2), null);
-		}
-		else {
-			g.drawImage(animation.getImage(), (int)(x+xmap-width/2+width), (int)(y+ymap-height/2), -width, height, null);
-		}
-	}
-	
+        // draw flaming projectiles
+        for (int i=0; i < projectiles.size(); i++){
+            projectiles.get(i).draw(g);
+        }
+
+        // draw player
+        if(facingRight) {
+            g.drawImage(animation.getImage(), (int)(x+xmap-width/2), (int)(y+ymap-height/2), null);
+        }
+        else {
+            g.drawImage(animation.getImage(), (int)(x+xmap-width/2+width), (int)(y+ymap-height/2), -width, height, null);
+        }
+    }
+
 }
