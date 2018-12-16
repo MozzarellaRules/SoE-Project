9fc81223dc47d3aa032e1bb8e76e27690ce9c40e
MozzarellaRules <viscontifrancesco95@tiscali.it>
2018-12-16 18:41:27 +0100

    Create EnemyWaterShark.java
    Copy pasted the octopus one just to have a base to work on. Has its own sprite + numFrames + animation speed.
    Its Y should never change

---
+++ src/entity/dynamic/EnemyWaterShark.java
@@ -0,0 +1,63 @@
+package entity.dynamic;
+
+import entity.DynamicSprite;
+import entity.ImageAnimator;
+import entity.strategy.StrategyFactory;
+import tilemap.TileMap;
+
+import java.awt.*;
+
+public class EnemyWaterShark extends DynamicSprite {
+    private int DEFAULT_ROW = 0;
+    private final int[] numFrames = {8};
+    private boolean up ;
+
+    public EnemyWaterShark(TileMap tm) {
+        super(tm);
+
+        setStrategyX(StrategyFactory.getInstance().getStopStrategyX());
+        setStrategyY(StrategyFactory.getInstance().getSwimDownStrategyY()); // Initially the shark is moving left
+
+        up = false ; // This boolean value shows if the shark is moving left or right
+
+
+        loadSpriteAsset(numFrames, "/Enemies/Shark.png");
+
+        // Animate sprite
+        imageAnimator = new ImageAnimator();
+        imageAnimator.setFrames(getSprites().get(DEFAULT_ROW));
+        imageAnimator.setDelay(100);
+    }
+
+    @Override
+    public void update() {
+
+        getNextDelta();
+        checkTileMapCollision();
+
+        if(getDy()==0){ // collision detected
+            if(up){
+                up = false;
+                this.setStrategyY(StrategyFactory.getInstance().getSwimDownStrategyY());
+            }else {
+                up = true;
+                this.setStrategyY(StrategyFactory.getInstance().getSwimUpStrategyY());
+            }
+        }
+
+        imageAnimator.update();
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        g.drawImage(
+                imageAnimator.getImage(),
+                (int)(getX()+tileMap.getX()-width/2),
+                (int)(getY()+tileMap.getY()-height/2),
+                width,
+                height,
+                null);
+    }
+
+}
+
