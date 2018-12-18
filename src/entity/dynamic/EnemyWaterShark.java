f48050d0f606f515d776bce68562a20fdf1d1cee
GingerKibbles <marcocapozzo@yahoo.it>
2018-12-18 23:07:46 +0100

    Right movement of the Shark

--- src/entity/dynamic/EnemyWaterShark.java
+++ src/entity/dynamic/EnemyWaterShark.java
@@ -18,8 +18,11 @@ public class EnemyWaterShark extends DynamicSprite {
     public EnemyWaterShark(TileMap tm) {
         super(tm);
 
-        setStrategyX(StrategyFactory.getInstance().getMoveStrategyX());
-        setStrategyY(StrategyFactory.getInstance().getStopStrategyY()); // Initially the shark is moving left
+        setFacingRight(false);
+        setMovingRight(false);
+
+        setStrategyX(StrategyFactory.getInstance().getMoveStrategyX()); // Initially the shark is moving to the left
+        setStrategyY(StrategyFactory.getInstance().getStopStrategyY());
 
         factorX = 1.0;
         factorY= 0;
@@ -37,23 +40,53 @@ public class EnemyWaterShark extends DynamicSprite {
     public void update() {
 
 
-        setNextDelta(factorX,factorY);
-        checkTileMapCollision();
+        setNextDelta(factorX,factorY); //I prossimi dx e dy che dovrebbe assumere lo shark
+        checkTileMapCollision(); // Qui dato che i tile al di sotto dello squalo potrebbero essere non bloccanti la strategy viene settata automaticamante
+        //a MoveStrategyY con falling a true
         setStrategyY(StrategyFactory.getInstance().getStopStrategyY());
 
+
+        if (getDx() == 0) { // Not moving -> collision detected
+            // Revert facing and moving strategy
+            if(isFacingRight()) {
+                setMovingRight(false);
+                setFacingRight(false);
+            }
+            else {
+                setMovingRight(true);
+                setFacingRight(true);
+            }
+
+        }
+
+
         imageAnimator.update();
+
     }
 
     @Override
     public void draw(Graphics2D g) {
+
+        if(!isFacingRight()){
         g.drawImage(
                 imageAnimator.getImage(),
                 (int)(getX()+tileMap.getX()-width/2),
                 (int)(getY()+tileMap.getY()-height/2),
                 width,
                 height,
-                null);
+                null);}
+         else {
+                g.drawImage(
+                        imageAnimator.getImage(),
+                        (int)(getX()+tileMap.getX()-width/2+width),
+                        (int)(getY()+tileMap.getY()-height/2),
+                        -width,
+                        height,
+                        null);
+            }
+
     }
+
 
 }
 
