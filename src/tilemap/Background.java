d4c665ed169de4eee3ae6692a5e02e4b8b7a55d2
GingerKibbles <marcocapozzo@yahoo.it>
2018-12-19 23:59:37 +0100

    Commit 4

--- src/tilemap/Background.java
+++ src/tilemap/Background.java
@@ -10,8 +10,6 @@ public class Background {
 	private BufferedImage image;
 	private double x;
 	private double y;
-	private double dx;
-	private double dy;
 	private double moveScale;
 	
 	public Background(String s, double ms) {
