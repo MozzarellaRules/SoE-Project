a3fac9806fd5578f43a7316bdbdaa379ed4e0ccf
soniastabile <44493759+soniastabile@users.noreply.github.com>
2018-12-19 16:48:52 +0100

    Add files via upload

--- testSrc/entity/ImageAnimatorTest.java
+++ testSrc/entity/ImageAnimatorTest.java
@@ -1,26 +1,39 @@
 package entity;
 import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+
 import entity.Sprite.*;
-import entity.objects.Ammo;
 
 import org.junit.*;
 import org.junit.jupiter.api.BeforeEach;
+
+import com.sun.prism.Image;
 
 import tilemap.TileMap;
 
 public class ImageAnimatorTest {
 	private ImageAnimator im;
 	private TileMap tm;
-	private Ammo ammo;
+	private Item it;
+	private BufferedImage i;
+	
 	@Before
 	public void setUp() {
 		im=new ImageAnimator();
 	}
+	
 	@Test
-	public void testhasplayedOnce() {
+	public void testHasPlayedOnce() {
 		assertFalse(im.hasPlayedOnce());
+	}		
+	
+	@Test
+	public void testGetFrame() {
+		im.setFrame(5);
+		assertEquals(im.getFrame(),5);
 	}		
 }
 	
