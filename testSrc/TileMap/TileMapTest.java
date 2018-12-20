de6959bba456c6a603f5456394315285287fbc1f
Pasquale Celella <pasquale.celella@gmail.com>
2018-12-20 09:32:39 +0100

    Fix bug demo

--- testSrc/TileMap/TileMapTest.java
+++ testSrc/TileMap/TileMapTest.java
@@ -1,27 +1,76 @@
-package TileMap;
+package tilemap;
 
+import gamestate.LevelOneState;
+import gamestate.LevelTwoState;
 import main.Game;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import tilemap.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 class TileMapTest extends Game {
     private TileMap tm;
-
     @BeforeEach
     void setUp() {
-        tm = new TileMap(32);
+       tm = new TileMap(32);
     }
 
     @Test
-    void testLoadTiles() {
-        tm.loadTiles("path_not_existing");
+    void testMapLevel1() {
+        tm.loadTiles(LevelOneState.TILESET_PATH);
+        tm.loadMap(LevelOneState.MAP_PATH);
+        assertEquals(tm.getNumRows(),54);
+        assertEquals(tm.getNumCols(),149);
+        
+        assertNotEquals(tm.getNumRows(), 0);
+        assertNotEquals(tm.getNumCols(), 0);
 
-        assertEquals(tm.getNumRows(), 0);
-        assertEquals(tm.getNumCols(), 0);
-
+        
+        /*assertThrows(NullPointerException.class, () -> {
+        tm.loadTiles("path_not_existing");      
+        }); */  
+        
     }
-
+    
+    @Test
+    void testMapLevel2() {
+    	tm.loadTiles(LevelTwoState.TILESET_PATH);
+    	tm.loadMap(LevelTwoState.MAP_PATH);
+    	//assertEqualsPerLeRighe
+    	//assertEqualsPerLeColonne
+    	assertNotEquals(tm.getNumRows(), 0);
+        assertNotEquals(tm.getNumCols(), 0);
+    }
+    
+    @Test
+    void testGetTypeLevel1() {
+    	 tm.loadTiles(LevelOneState.TILESET_PATH);
+         tm.loadMap(LevelOneState.MAP_PATH);
+         assertThrows(Exception.class, () -> {
+        	 tm.getType(-1, -1);     	 
+             });
+         assertThrows(Exception.class, () -> {
+        	 tm.getType(tm.getNumRows(),tm.getNumCols());     	 
+             });
+         
+    }
+    
+    @Test
+    void testGetTypeLevel2() {
+    	 tm.loadTiles(LevelTwoState.TILESET_PATH);
+         tm.loadMap(LevelTwoState.MAP_PATH);
+         assertThrows(Exception.class, () -> {
+        	 tm.getType(-1, -1);     	 
+             });
+         assertThrows(Exception.class, () -> {
+        	 tm.getType(tm.getNumRows(),tm.getNumCols());     	 
+             });
+         
+    }
+    
+    
+ 
 }
